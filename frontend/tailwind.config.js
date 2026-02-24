cec666b3b3a2bef429bd1e4fabfa36a6c93ac682
stepan <stevee1017@icloud.com>
2026-02-24 20:08:16 +0100

    done

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
     darkMode: ["class"],
     content: [
     "./src/**/*.{js,jsx,ts,tsx}",
-    "./public/index.html"
+    "./public/index.cra.html"
   ],
   theme: {
   	extend: {
